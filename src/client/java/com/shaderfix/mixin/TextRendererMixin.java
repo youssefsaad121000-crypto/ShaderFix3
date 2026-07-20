f43a1f700eb3cc4221c58129c768c4b21ee19690
youssefsaad121000-crypto <youssefsaad121000@gmail.com>
2026-07-20 11:18:03 +0300

    Create TextRendererMixin.java

---
+++ src/client/java/com/shaderfix/mixin/TextRendererMixin.java
@@ -0,0 +1,9 @@
+package com.shaderfix.mixin;
+
+import net.minecraft.client.font.TextRenderer;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(TextRenderer.class)
+public class TextRendererMixin {
+
+}
