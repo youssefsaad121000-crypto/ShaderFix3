fd008dff86e671b8f06302bd974828b59c914ab0
youssefsaad121000-crypto <youssefsaad121000@gmail.com>
2026-07-20 12:59:04 +0300

    Update ShaderFixClient.java

--- src/client/java/com/shaderfix/ShaderFixClient.java
+++ src/client/java/com/shaderfix/ShaderFixClient.java
@@ -1,16 +1,11 @@
 package com.shaderfix;
 
 import net.fabricmc.api.ClientModInitializer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class ShaderFixClient implements ClientModInitializer {
 
-    public static final Logger LOGGER =
-            LoggerFactory.getLogger("ShaderFix");
-
     @Override
     public void onInitializeClient() {
-        LOGGER.info("ShaderFix 0.1 Loaded!");
+        System.out.println("[ShaderFix] Loaded!");
     }
 }
